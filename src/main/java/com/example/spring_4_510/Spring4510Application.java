17771263cb3d56c50010b245f5a6b78fb212462d
lizhao <lukehatton685@mail.com>
2021-11-08 18:12:22 +0800

    Initial commit

---
+++ src/main/java/com/example/spring_4_510/Spring4510Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_4_510;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4510Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring4510Application.class, args);
+    }
+
+}
